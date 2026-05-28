6fcf0d82c0de99daffef65b468b75f1efbe4ac25
Ayush Patel <ayushpatel5779@gmail.com>
2026-05-28 15:05:26 +0530

    fix CORS open all origins

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -3,60 +3,35 @@ console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 const express = require("express");
 const cors = require("cors");
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const mongoose = require("mongoose");
 
 const app = express();
 
-const allowedOrigins = [
-  "https://hillaryush-portfolio.vercel.app",
-  "https://portfolio-backend-mauve-nine.vercel.app",
-  "http://localhost:5500",
-  "http://localhost:3000",
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
-app.use("/contact", require("./routes/contact"));
-app.use("/api", require("./routes/contact"));
-app.use("/api/contact", require("./routes/contact"));
-app.use("/api", require("./routes/adminAuth"));
-app.listen(5000, () =>
-  console.log("✅ Backend running on http://localhost:5000")
-);
 app.get("/", (req, res) => {
   res.send("🚀 Backend is running");
 });
-const adminAuth = require("./routes/adminAuth");
-
-app.use("/api/admin", adminAuth);
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ status: 'alive' });
 });
 
-const adminMessages = require("./routes/adminMessages");
+app.use("/api/contact", require("./routes/contact"));
+app.use("/contact", require("./routes/contact"));
 
+const adminAuth = require("./routes/adminAuth");
+app.use("/api/admin", adminAuth);
+app.use("/api", adminAuth);
+
+const adminMessages = require("./routes/adminMessages");
 app.use("/api/admin", adminMessages);
-const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+app.listen(5000, () =>
+  console.log("✅ Backend running on http://localhost:5000")
+);
